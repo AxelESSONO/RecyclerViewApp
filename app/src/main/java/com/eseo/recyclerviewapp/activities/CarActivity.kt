07addc74513ee9681ff99a2ee2fccef79c432d73
axelessono <naxelcdric@gmail.com>
2024-03-20 09:27:47 +0100

    Initial commit

---
+++ app/src/main/java/com/eseo/recyclerviewapp/activities/CarActivity.kt
@@ -0,0 +1,12 @@
+package com.eseo.recyclerviewapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eseo.recyclerviewapp.R
+
+class CarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car)
+    }
+}
